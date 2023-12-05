2c8430510ad6612bce637ec9a0e792b5ea0d911e
Rico Kunz <rico.kunz@usu.com>
2023-12-05 07:34:10 +0100

    start preparation next days

---
+++ src/test/kotlin/day05/Day05Test.kt
@@ -0,0 +1,38 @@
+package day05
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day05Test {
+  private val demoDay = Day05(5, true)
+  private val lifeDay = Day05(5, false)
+
+  @Test
+  fun `calculate solution for demo data part one`() {
+    val expectedDemo = 0
+    val actualDemo = demoDay.calcPartOne()
+    Assertions.assertEquals(expectedDemo, actualDemo)
+  }
+
+  @Test
+  fun `calculate solution for life data part one`() {
+    // Life-Data
+    val expectedLife = 0
+    val actualLife = lifeDay.calcPartOne()
+    Assertions.assertEquals(expectedLife, actualLife)
+  }
+
+  @Test
+  fun `calculate solution for demo data part two`() {
+    val expectedDemo = 0
+    val actualDemo = demoDay.calcPartTwo()
+    Assertions.assertEquals(expectedDemo, actualDemo)
+  }
+
+  @Test
+  fun `calculate solution for life data part two`() {
+    val expectedLife = 0
+    val actualLife = lifeDay.calcPartTwo()
+    Assertions.assertEquals(expectedLife, actualLife)
+  }
+}
---
+++ src/test/kotlin/day06/Day06Test.kt
@@ -0,0 +1,38 @@
+package day06
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+  private val demoDay = Day06(6, true)
+  private val lifeDay = Day06(6, false)
+
+  @Test
+  fun `calculate solution for demo data part one`() {
+    val expectedDemo = 0
+    val actualDemo = demoDay.calcPartOne()
+    Assertions.assertEquals(expectedDemo, actualDemo)
+  }
+
+  @Test
+  fun `calculate solution for life data part one`() {
+    // Life-Data
+    val expectedLife = 0
+    val actualLife = lifeDay.calcPartOne()
+    Assertions.assertEquals(expectedLife, actualLife)
+  }
+
+  @Test
+  fun `calculate solution for demo data part two`() {
+    val expectedDemo = 0
+    val actualDemo = demoDay.calcPartTwo()
+    Assertions.assertEquals(expectedDemo, actualDemo)
+  }
+
+  @Test
+  fun `calculate solution for life data part two`() {
+    val expectedLife = 0
+    val actualLife = lifeDay.calcPartTwo()
+    Assertions.assertEquals(expectedLife, actualLife)
+  }
+}
