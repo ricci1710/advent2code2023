c42f75df3589af2f1e7a40ec2e808c5cd3183764
ricci1710 <ricci1710@gmail.com>
2023-12-19 13:02:46 +0100

    code preparation day 18 part one

--- src/main/kotlin/day18/Day18.kt
+++ src/main/kotlin/day18/Day18.kt
@@ -2,24 +2,72 @@ package day18
 
 import base.Day
 
-class Day18(dayNumber: Int, loadDemoData: Boolean) : Day(dayNumber, loadDemoData) {
-  // <Zeile-Y, <MinX, MaxX>> => Distanz = Abs(MinX) + Abs(MaxX)
-  private val lagoonPlan: MutableMap<Int, Pair<Int, Int>> = mutableMapOf()
-  fun parseCubeInfo(cmd: String): LagoonCubes {
-    val cmdParts = cmd.split(" ")
-    return LagoonCubes(
-      cmdParts[0],
-      cmdParts[1].toInt(),
-      cmdParts[2].substring(1, cmdParts[2].length - 1)
-    )
-  }
-
-  private fun digger(startPoint: Pair<Int, Int>, lagoonCubes: LagoonCubes) {
-    when (lagoonCubes.direction) {
-      "R" -> {
-      }
+class Day18(dayNumber: Int, loadDemoData: Boolean, postFix: String = "") : Day(dayNumber, loadDemoData, postFix) {
+    // <Zeile-Y, [MinX, MaxX]> => Distanz = Abs(MinX) + Abs(MaxX)
+    private val lagoonPlan: MutableMap<Int, MutableSet<Int>> = mutableMapOf()
+    fun parseCubeInfo(cmd: String): LagoonCubes {
+        val cmdParts = cmd.split(" ")
+        return LagoonCubes(
+            cmdParts[0],
+            cmdParts[1].toInt(),
+            cmdParts[2].substring(1, cmdParts[2].length - 1)
+        )
     }
-  }
+
+    fun digger(diggerPoint: DiggerPoint, lagoonCubes: LagoonCubes): DiggerPoint {
+        when (lagoonCubes.direction) {
+            "R" -> {
+                diggerPoint.column += lagoonCubes.hub
+                val lineInfo = lagoonPlan[diggerPoint.row]
+                if (lineInfo != null) {
+                    lineInfo.add(diggerPoint.column)
+                } else {
+                    lagoonPlan[diggerPoint.row] = mutableSetOf(diggerPoint.column)
+                }
+            }
+
+            "L" -> {
+                diggerPoint.column -= lagoonCubes.hub
+                val lineInfo = lagoonPlan[diggerPoint.row]
+                if (lineInfo != null) {
+                    lineInfo.add(diggerPoint.column)
+                } else {
+                    lagoonPlan[diggerPoint.row] = mutableSetOf(diggerPoint.column)
+                }
+            }
+
+            "U" -> {
+                for (rowIdx in 1..lagoonCubes.hub) {
+                    val lineIdx = diggerPoint.row - rowIdx
+                    val lineInfo = lagoonPlan[lineIdx]
+                    if (lineInfo != null) {
+                        lineInfo.add(diggerPoint.column)
+                    } else {
+                        lagoonPlan[lineIdx] = mutableSetOf(diggerPoint.column)
+                    }
+                }
+                diggerPoint.row -= lagoonCubes.hub
+            }
+
+            "D" -> {
+                for (rowIdx in 1..lagoonCubes.hub) {
+                    val lineIdx = diggerPoint.row + rowIdx
+                    val lineInfo = lagoonPlan[lineIdx]
+                    if (lineInfo != null) {
+                        lineInfo.add(diggerPoint.column)
+                    } else {
+                        lagoonPlan[lineIdx] = mutableSetOf(diggerPoint.column)
+                    }
+                }
+                diggerPoint.row += lagoonCubes.hub
+            }
+        }
+
+        return diggerPoint
+    }
+
+    fun getLagoonPlan() = lagoonPlan
+
 //  private fun digger(startPoint: Pair<Int, Int>, lagoonCubes: LagoonCubes, lagoon: Array2d<Char>): Pair<Int, Int> {
 //    return when (lagoonCubes.direction) {
 //      "R" -> Pair(startPoint.first, startPoint.second + lagoonCubes.hub)
@@ -32,35 +80,47 @@ class Day18(dayNumber: Int, loadDemoData: Boolean) : Day(dayNumber, loadDemoData
 //    }
 //  }
 
-  private fun diggerLagoon() {
-    var diggerPoint = Pair<Int, Int>(0, 0);
-    storeData.forEach { line ->
-      val lagoonCubes = parseCubeInfo(line)
-      digger(diggerPoint, lagoonCubes)
-      println(diggerPoint.toString())
+    private fun diggerLagoon(diggerPoint: DiggerPoint) {
+        var nextDiggerPoint = diggerPoint
+        storeData.forEach { line ->
+            val lagoonCubes = parseCubeInfo(line)
+            nextDiggerPoint = digger(nextDiggerPoint, lagoonCubes)
+        }
     }
 
-    println(lagoonPlan.toString())
-  }
+    /**
+     * Berechnung der ersten Teilaufgabe
+     * @returns {Int}
+     * @override
+     */
+    override fun calcPartOne(): Int {
+        // Init digger start
+        val diggerPoint = DiggerPoint(0, 0)
+        lagoonPlan[diggerPoint.row] = mutableSetOf(0)
 
-  /**
-   * Berechnung der ersten Teilaufgabe
-   * @returns {Int}
-   * @override
-   */
-  override fun calcPartOne(): Int {
-    diggerLagoon()
-    return 0
-  }
+        diggerLagoon(diggerPoint)
+        println("$diggerPoint $lagoonPlan")
+        var result = 0
+        lagoonPlan.forEach { line ->
+            val sortedList = line.value.sorted()
+            result += if (sortedList.size % 2 == 0) {
+                sortedList.last() - sortedList.first() + 2
+            } else {
+                sortedList.last() - sortedList.first() + 1
+            }
+        }
+        return result
+    }
 
-  /**
-   * Berechnung der zweiten Teilaufgabe
-   * @returns {Int}
-   * @override
-   */
-  override fun calcPartTwo(): Int {
-    return 0
-  }
+    /**
+     * Berechnung der zweiten Teilaufgabe
+     * @returns {Int}
+     * @override
+     */
+    override fun calcPartTwo(): Int {
+        return 0
+    }
 }
 
 data class LagoonCubes(val direction: String, val hub: Int, val rgb: String)
+data class DiggerPoint(var row: Int, var column: Int)
--- src/test/kotlin/day18/Day18Test.kt
+++ src/test/kotlin/day18/Day18Test.kt
@@ -4,43 +4,72 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day18Test {
-  private val demoDay = Day18(18, true)
-  private val lifeDay = Day18(18, false)
+    private val demoDay = Day18(18, true)
+    private val lifeDay = Day18(18, false)
 
-  @Test
-  fun `calculate solution for demo data part one`() {
-    val expectedDemo = 0
-    val actualDemo = demoDay.calcPartOne()
-    Assertions.assertEquals(expectedDemo, actualDemo)
-  }
+    @Test
+    fun `calculate solution for demo data part one`() {
+        val expectedDemo = 68
+        val actualDemo = demoDay.calcPartOne()
+        Assertions.assertEquals(expectedDemo, actualDemo)
+    }
 
-  @Test
-  fun `calculate solution for life data part one`() {
-    // Life-Data
-    val expectedLife = 0
-    val actualLife = lifeDay.calcPartOne()
-    Assertions.assertEquals(expectedLife, actualLife)
-  }
+    @Test
+    fun `calculate solution for life data part one`() {
+        // Life-Data
+        val expectedLife = 70158 // 70157 to high
+        val actualLife = lifeDay.calcPartOne()
+        Assertions.assertEquals(expectedLife, actualLife)
+    }
 
-  @Test
-  fun `calculate solution for demo data part two`() {
-    val expectedDemo = 0
-    val actualDemo = demoDay.calcPartTwo()
-    Assertions.assertEquals(expectedDemo, actualDemo)
-  }
+    @Test
+    fun `calculate solution for demo data part two`() {
+        val expectedDemo = 0
+        val actualDemo = demoDay.calcPartTwo()
+        Assertions.assertEquals(expectedDemo, actualDemo)
+    }
 
-  @Test
-  fun `calculate solution for life data part two`() {
-    val expectedLife = 0
-    val actualLife = lifeDay.calcPartTwo()
-    Assertions.assertEquals(expectedLife, actualLife)
-  }
+    @Test
+    fun `calculate solution for life data part two`() {
+        val expectedLife = 0
+        val actualLife = lifeDay.calcPartTwo()
+        Assertions.assertEquals(expectedLife, actualLife)
+    }
 
-  @Test
-  fun parseCubeInfo() {
-    val actualDemo = demoDay.parseCubeInfo("D 12 (#219443)")
-    Assertions.assertEquals("D", actualDemo.direction)
-    Assertions.assertEquals(12, actualDemo.hub)
-    Assertions.assertEquals("#219443", actualDemo.rgb)
-  }
+    @Test
+    fun parseCubeInfo() {
+        val actualDemo = demoDay.parseCubeInfo("D 12 (#219443)")
+        Assertions.assertEquals("D", actualDemo.direction)
+        Assertions.assertEquals(12, actualDemo.hub)
+        Assertions.assertEquals("#219443", actualDemo.rgb)
+    }
+
+    @Test
+    fun `digger on command line`() {
+        val diggerPoint = DiggerPoint(0, 0)
+        val nextDiggerPoint = demoDay.digger(diggerPoint, LagoonCubes("R", 5, "#219443"))
+        Assertions.assertEquals(0, nextDiggerPoint.row)
+        Assertions.assertEquals(5, nextDiggerPoint.column)
+
+        val lagoonPlan = demoDay.getLagoonPlan()
+        Assertions.assertEquals(1, lagoonPlan[0]!!.size)
+    }
+
+    /**
+     * ###.###
+     * #.###.#
+     * #.....#
+     * ##..###
+     * .#..#..
+     * .#..###
+     * .#....#
+     * .######
+     */
+    @Test
+    fun `test with own better datas`() {
+        val expectedDemo = 49
+        val demoDay = Day18(18, true, "a")
+        val actualDemo = demoDay.calcPartOne()
+        Assertions.assertEquals(expectedDemo, actualDemo)
+    }
 }
